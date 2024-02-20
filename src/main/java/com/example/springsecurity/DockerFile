3662b4e368b2dbb89473184550f3b7b932afd800
ikrame <elkailech.ikrame@gmail.com>
2024-02-20 16:15:20 +0100

    security

---
+++ src/main/java/com/example/springsecurity/DockerFile
@@ -0,0 +1,4 @@
+FROM openjdk:8-jdk-alpine
+WORKDIR /app
+COPY target/LabXpert-0.0.1-SNAPSHOT.jar /app/app-1.0.0.jar
+ENTRYPOINT ["java","-jar","app-1.0.0.jar"]
---
+++ src/main/java/com/example/springsecurity/controller/LoginController.java
@@ -0,0 +1,24 @@
+package yass.jouao.labx.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/user")
+    public String getUser() {
+        return "Welcome, User";
+    }
+
+    @GetMapping("/admin")
+    public String getAdmin() {
+        return "Welcome, Admin";
+    }
+
+    @GetMapping("/")
+    public String getGitHub() {
+        return "Welcome, GitHub user";
+    }
+
+}
